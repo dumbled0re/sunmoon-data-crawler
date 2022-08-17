452ad2c99d07aaea193f88271198d31272d1c1ba
dumbled0re <s.r19990706@gmail.com>
2022-08-18 00:19:18 +0900

    add: spider追加

--- spider/items.py
+++ spider/items.py
@@ -1,12 +1,14 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from itemloaders.processors import TakeFirst
+from scrapy.item import Field, Item
 
 
-class SpiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class SemesterItem(Item):
+    start_date = Field(output_processor=TakeFirst())
+    end_date = Field(output_processor=TakeFirst())
+    crawled_url = Field(output_processor=TakeFirst())
+
+
+class HolidayItem(Item):
+    start_date = Field(output_processor=TakeFirst())
+    end_date = Field(output_processor=TakeFirst())
+    crawled_url = Field(output_processor=TakeFirst())
--- spider/spiders/sunmoon_spider.py
+++ spider/spiders/sunmoon_spider.py
@@ -1,16 +1,48 @@
+from scrapy.http.response.html import HtmlResponse
 from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
 from scrapy.spiders import CrawlSpider, Rule
 
+from spider.items import HolidayItem, SemesterItem
+from spider.utils.spider_utils import SpiderUtils
 
-class SunmoonSpiderSpider(CrawlSpider):
-    name = "sunmoon_spider"
-    allowed_domains = ["lily.sunmoon.ac.kr"]
-    start_urls = ["https://lily.sunmoon.ac.kr/"]
+
+class SunmoonSpider(CrawlSpider):
+    name: str = "sunmoon_spider"
+    allowed_domains: list[str] = ["sunmoon.ac.kr"]
+    custom_settings: dict[str, dict[str, int]] = {
+        "ITEM_PIPELINES": {
+            "spider.pipelines.SpiderPipeline": 300,
+        }
+    }
+
     rules = (
+        # NOTE: 学期中の時刻表
         Rule(
-            LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True
+            LinkExtractor(restrict_text="학기 시간표"),
+            callback="semester_item",
+            follow=False,
+        ),
+        # NOTE: 休みの時刻表
+        Rule(
+            LinkExtractor(restrict_text="방학 시간표"),
+            callback="holiday_item",
+            follow=False,
         ),
     )
 
-    def parse_item(self) -> None:
-        pass
+    def semester_item(self, response: HtmlResponse) -> ItemLoader:
+        semester_item: ItemLoader = SpiderUtils.add_value_to_semester_item(
+            semester_item=ItemLoader(item=SemesterItem(), response=response),
+            response=response,
+        )
+
+        return semester_item.load_item()
+
+    def holiday_item(self, response: HtmlResponse) -> ItemLoader:
+        holiday_item: ItemLoader = SpiderUtils.add_value_to_holiday_item(
+            holiday_item=ItemLoader(item=HolidayItem(), response=response),
+            response=response,
+        )
+
+        return holiday_item.load_item()
