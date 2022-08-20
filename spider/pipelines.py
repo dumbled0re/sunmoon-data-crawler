a549e688a324c4080b5bed5dfdbbe1818a097aa9
dumbled0re <s.r19990706@gmail.com>
2022-08-20 11:50:06 +0900

    add: 更新用のパイプライン追加

--- spider/pipelines.py
+++ spider/pipelines.py
@@ -1,4 +1,5 @@
 import time
+from typing import Optional
 
 from scrapy import Spider
 
@@ -84,4 +85,110 @@ class SpiderPipeline:
         )
         self._slack.slack_notify(
             f"[Sunmoon] 追加件数{self._num_data}件 収集時間: {elapsed_time_format}"
+        )
+
+
+class SpiderPipelineUpdate(SpiderPipeline):
+    """スパイダーから引き渡されたアイテムオブジェクトを処理するパイプライン"""
+
+    def open_spider(self, spider: Spider) -> None:
+        """スパイダーの起動前に呼び出されるメソッド
+
+        Args:
+            spider (Spider): スパイダークラスのインスタンス
+        """
+
+        self.start_time = time.time()
+        self._slack.slack_notify("[Sunmoon] 更新処理を開始")
+        spider.start_urls = self._sunmoon_bus.select_all_crawled_url()
+        self.db_data = self._sunmoon_bus.select_all()
+        self.update_data_list: list[dict[str, str]] = []
+
+    def process_item(self, item: SunmoonItem, spider: Spider) -> SunmoonItem:
+        """DB保存前にインスタンスごとにバルクに保存し、DBへのアクセス頻度を調整するメソッド(Pipelineにデータが渡される時に実行される)
+
+        Args:
+            item (SunmoonItem): スパイダーから返されるアイテムオブジェクト
+            spider (Spider): スパイダークラスのインスタンス
+        Returns:
+            item: DBに保存するアイテムオブジェクト
+        """
+
+        try:
+            db_data = self.__get_data_from_db(item)
+            if db_data:
+                if self.__check_change_data(db_data, item):
+                    update_data = dict(item)
+                    update_data["db_id"] = db_data["db_id"]
+                    self.update_data_list.append(update_data)
+                return item
+            raise ValueError("ValueError")
+        except ValueError as error:
+            logger.exception(error, extra=dict(spider=spider))
+            raise
+        except Exception as error:
+            logger.exception(error, extra=dict(spider=spider))
+            raise
+
+    def __get_data_from_db(
+        self, item: SunmoonItem
+    ) -> Optional[dict[str, str]]:
+        """DBから特定のデータを取得
+
+        Args:
+            item (SunmoonItem): スパイダーから返されるアイテムオブジェクト
+        Returns:
+            dict : DBデータ
+        """
+        for data_dict in self.db_data:
+            if data_dict["bus_type"] == item["bus_type"]:
+                return data_dict
+        return None
+
+    def __check_change_data(
+        self, db_data: dict[str, str], item: SunmoonItem
+    ) -> bool:
+        """既存のデータに対して変更が合ったかどうかをBOOL値で返す
+
+        Args:
+            db_data (dict): DBのデータ
+            item (SunmoonItem): スパイダーから返されるアイテムオブジェクト
+        Returns:
+            bool: 真偽値
+        """
+        if (
+            db_data["start_date"] != item["start_date"]
+            or db_data["end_date"] != item["end_date"]
+        ):
+            return True
+        return False
+
+    def close_spider(self, spider: Spider) -> None:
+        """スパイダー終了時に呼び出されるメソッド
+
+        Args:
+            spider (Spider): スパイダークラスのインスタンス
+        """
+
+        try:
+            if self.update_data_list:
+                self._sunmoon_bus.bulk_update(self.update_data_list)
+                self._num_data += len(self.update_data_list)
+        except Exception as error:
+            logger.exception(error, extra=dict(spider=spider))
+            raise
+
+        elapsed_time = int(time.time() - self.start_time)
+
+        elapsed_hour = elapsed_time // 3600
+        elapsed_minute = (elapsed_time % 3600) // 60
+        elapsed_second = elapsed_time % 3600 % 60
+
+        elapsed_time_format = f"{str(elapsed_hour).zfill(2)}:{str(elapsed_minute).zfill(2)}:{str(elapsed_second).zfill(2)}"
+
+        logger.info(
+            f"[Sunmoon] 更新件数{self._num_data}件 更新時間: {elapsed_time_format}"
+        )
+        self._slack.slack_notify(
+            f"[Sunmoon] 更新件数{self._num_data}件 更新時間: {elapsed_time_format}"
         )
